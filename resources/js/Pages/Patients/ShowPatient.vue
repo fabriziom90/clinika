ab10135afb8f4e1150c558339fcb92547c51cbda
fabriziom90 <fabriziomastrobattista@gmail.com>
2026-02-09 11:19:46 +0100

    [UPD] Refactoring show patient's details

--- resources/js/Pages/Patients/ShowPatient.vue
+++ resources/js/Pages/Patients/ShowPatient.vue
@@ -2,10 +2,16 @@
 import { Head, Link } from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { formatDate } from "@/utilities/formatDateFunction";
+import { ref } from "vue";
 
 const props = defineProps({
     patient: Object,
 });
+
+const detailsOpen = ref(false);
+
+const showDetail = () => detailsOpen.value = !detailsOpen.value;
+
 </script>
 <template lang="">
     <Head title="Dettaglio paziente"></Head>
@@ -30,71 +36,89 @@ const props = defineProps({
                 </div>
                 <hr />
                 <div class="col-12">
-                    <h3>Anagrafica paziente</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Nome</label>
-                    <p>{{ patient.name }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Cognome</label>
-                    <p>{{ patient.surname }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Data di nascita</label>
-                    <p>{{ formatDate(patient.birthday) }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Città di nascita</label>
-                    <p>{{ patient.birth_city }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Nazionalità</label>
-                    <p>
-                        {{
-                            patient.nationality === null
-                                ? "Nazionalità non valorizzata"
-                                : patient.nationality.name
-                        }}
-                    </p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Sesso</label>
-                    <p>{{ patient.genre === "m" ? "Uomo" : "Donna" }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Codice fiscale</label>
-                    <p>{{ patient.personal_code }}</p>
+                    <div class="row">
+                        <div class="col-12">
+                            <h3>{{ patient.name }} {{ patient.surname }} <i class="fas" :class="detailsOpen == true ? 'fa-caret-up' : 'fa-caret-down'" @click="showDetail"></i></h3>
+                        </div>
+                        <div class="col-12" v-if="detailsOpen">
+                            <div class="bg-main-red text-white">
+                                <div class="row gy-3">
+                                    <div class="col-12">
+                                        <h3>Anagrafica dottore</h3>
+                                    </div>
+                                    
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label"
+                                            >Data di nascita</label
+                                        >
+                                        <p>{{ formatDate(patient.birthday) }}</p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label"
+                                            >Città di nascita</label
+                                        >
+                                        <p>{{ patient.birth_city }}</p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label">Nazionalità</label>
+                                        <p>
+                                            {{
+                                                patient.nationality === null
+                                                    ? "Nazionalità non valorizzata"
+                                                    : patient.nationality.name
+                                            }}
+                                        </p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label">Sesso</label>
+                                        <p>
+                                            {{ patient.genre === "m" ? "Uomo" : "Donna" }}
+                                        </p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label"
+                                            >Codice fiscale</label
+                                        >
+                                        <p>{{ patient.personal_code }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row gy-3">
+                                    <div class="col-12">
+                                        <h3>Residenza</h3>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label"
+                                            >Città di residenza</label
+                                        >
+                                        <p>{{ patient.city }}</p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label">Indirizzo</label>
+                                        <p>{{ patient.address }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row gy-3">
+                                    <div class="col-12">
+                                        <h3>Informazioni contatto</h3>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label">Email</label>
+                                        <p>{{ patient.email }}</p>
+                                    </div>
+                                    <div class="col-12 col-md-2">
+                                        <label for="" class="form-label">Telefono</label>
+                                        <p>{{ patient.phone }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
-            <hr />
-            <div class="row gy-3">
-                <div class="col-12">
-                    <h3>Residenza</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Città di residenza</label>
-                    <p>{{ patient.city }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Indirizzo</label>
-                    <p>{{ patient.address }}</p>
-                </div>
-            </div>
-            <hr />
-            <div class="row gy-3">
-                <div class="col-12">
-                    <h3>Informazioni contatto</h3>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Email</label>
-                    <p>{{ patient.email }}</p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <label for="" class="form-label">Telefono</label>
-                    <p>{{ patient.phone }}</p>
-                </div>
-            </div>
+            
         </div>
     </AuthenticatedLayout>
 </template>
@@ -105,6 +129,13 @@ const props = defineProps({
 h3,
 label {
     color: $mainRed;
+}
+
+.bg-main-red{
+    padding: 20px;
+    h3, label, hr{
+        color: #fff;
+    }
 }
 
 label {
